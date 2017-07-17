42887b52a7917e01150587ccfc5bb4912d28c5dd
Michael Meister (@Zenbook) <michael.meister.01@gmail.com>
2017-07-17 23:37:12 +0200

    further develop enum ChatFormat

--- converter/Chattex/src/de/chatPrinter/enums/ChatFormat.java
+++ converter/Chattex/src/de/chatPrinter/enums/ChatFormat.java
@@ -1,36 +1,25 @@
 package de.chatPrinter.enums;
 
-import java.text.DateFormat;
+import java.time.format.DateTimeFormatter;
+
 
 public enum ChatFormat {
-	WHATSAPP(){
-		@Override
-		void setConstants(){
-			regex = "";
-			dateFormat = "";
-		}
-	},
-	SKYPE(){
-		@Override
-		void setConstants(){
-			regex = "";
-			dateFormat = "";
-		}
-	};
+	WHATSAPP("(?<date>(?:\\d\\d\\.){2}\\d\\d, \\d\\d:\\d\\d) - (?<author>[^:]+): (?<message>.*)",
+			DateTimeFormatter.ofPattern("dd.MM.yy, HH:mm"), ""
+			),
+	SKYPE("\\[(?<date>(?:\\d\\d:){2}\\d\\d)\\] (?<author>[^:]+): (?<message>.*)",
+			DateTimeFormatter.ofPattern("HH:mm:ss"), //TODO pattern anpassen, damit es ausgeschriebenen monat frisst
+			" [A-Z]\\w+, (\\d{1,2}\\. [A-Z]\\w+ \\d{4})$"
+			);
 	
 	public final String REGEX;
-	public final String DATE_FORMAT;
-	String regex, dateFormat;
+	public final DateTimeFormatter DATE_FORMAT;
+	public final String DATE_REGEX;
 	
-	ChatFormat() {
-		setConstants();
+	private ChatFormat(String regex, DateTimeFormatter dateFormat, String dateRegex){
 		REGEX = regex;
 		DATE_FORMAT = dateFormat;
-	}
-	
-	void setConstants(){
-		regex = "";
-		dateFormat = "";
+		DATE_REGEX = dateRegex;
 	}
 
 }
